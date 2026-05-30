573a6c8193d5cb6936ced9d46bc30829ea34085b
badreddine55 <156625233+badreddine55@users.noreply.github.com>
2026-05-30 04:49:25 +0000

    last time touch code

--- src/logic_file.py
+++ src/logic_file.py
@@ -5,23 +5,25 @@ import numpy as np
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'llm_sdk'))
 from llm_sdk import Small_LLM_Model
 from src.parsing import Parser
-
+from itertools import zip_longest
 
 class FunctionsDict:
     def __init__(self):
         self.parser = Parser()
-        self.model = Small_LLM_Model() 
+        self.model = Small_LLM_Model()
+        self.functions = None
 
     def functions_list(self, path: str):
         new_list_functions = []
-        functions = self.parser.load_functions(path)
+        functions_names = []
+        self.functions = self.parser.load_functions(path)
 
-        for fun in functions:
+        for fun in self.functions:
             params = fun["parameters"]
             params_str = ", ".join(
                 f"{k}: {v['type']}" for k, v in params.items()
             )
-
+            functions_names.append(self.model.encode(fun['name']).tolist()[0])
             line = (
                 f"function_name: {fun['name']}({params_str}) "
                 f":returns({fun['returns']['type']}) "
@@ -30,7 +32,7 @@ class FunctionsDict:
 
             new_list_functions.append(line)
 
-        return new_list_functions
+        return (new_list_functions, functions_names)
 
     def prompt_list(self, path: str):
         new_list_prompt = []
@@ -61,10 +63,82 @@ class FunctionsDict:
             JSON response:
             {{\"function_name\": \""""
 
+
     def get_logits(self, input_ids):
-        logits = self.model.get_logits_from_input_ids(input_ids)
-        next_id = int(np.argmax(logits))
-        print(next_id)
+        return self.model.get_logits_from_input_ids(input_ids)
+
+    def extract_function_name(self, list_func_tokens, input_ids):
+        expected_fun = []
+
+        for token_group in zip_longest(*list_func_tokens):
+            allowed_tokens = set(
+                x for x in token_group
+                if x is not None
+            )
+
+            if len(allowed_tokens) == 1:
+                token = next(iter(allowed_tokens))
+            else:
+                logits = self.get_logits(input_ids)
+
+                for token_id in range(len(logits)):
+                    if token_id not in allowed_tokens:
+                        logits[token_id] = -float('inf')
+
+                token = np.argmax(logits)
+
+            expected_fun.append(token)
+            input_ids.append(token)
+
+            # break if exact match found
+            if expected_fun in list_func_tokens:
+                break
+
+        return expected_fun
+    def extract_function_params(self, function_name, input_ids):
+        input_ids.extend(self.model.encode('", "parameters": {').tolist()[0])
+        function = next(f for f in self.functions if f["name"] == function_name)
+        parameters = function["parameters"]
+        params = list(parameters.items())
+
+        for i, (param_name, param_info) in enumerate(params):
+            last_param = (i == len(params) - 1)
+
+            if i > 0:
+                input_ids.extend(self.model.encode(", ").tolist()[0])
+
+            input_ids.extend(self.model.encode(f'"{param_name}": ').tolist()[0])
+
+            if param_info["type"] == "string":
+                input_ids.extend(self.model.encode('"').tolist()[0])
+                while True:
+                    logits = self.get_logits(input_ids)
+                    token = int(np.argmax(logits))
+                    token_text = self.model.decode([token])
+                    if '"' in token_text:
+                        break
+                    input_ids.append(token)
+
+                input_ids.extend(self.model.encode('"').tolist()[0])
+
+            elif param_info["type"] == "number":
+                while True:
+                    logits = self.get_logits(input_ids)
+                    token = int(np.argmax(logits))
+                    token_text = self.model.decode([token])
+                    if any(c in token_text for c in {",", "}"}):
+                        digit_part = token_text.split(",")[0].split("}")[0].strip()
+                        if digit_part:
+                            input_ids.extend(self.model.encode(digit_part).tolist()[0])
+                        break
+                    input_ids.append(token)
+
+        input_ids.extend(self.model.encode("}}").tolist()[0])
+        print(self.model.decode(input_ids))
+        return input_ids
+
+    
+
 
 
         
--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
 from src.parsing import Parser
 from src.logic_file import FunctionsDict
-from llm_sdk import Small_LLM_Model
 path_function_file = "data/input/functions_definition.json"
 path_prompts_file = "data/input/function_calling_tests.json"
-model = Small_LLM_Model() 
 fun = FunctionsDict()
 
 
-prompt = fun.prompt_list(path_prompts_file)[3]
-list_func = fun.functions_list(path_function_file)
-input_ids =  model.encode(fun.build_prompt(prompt, list_func)).tolist()[0]
-fun.get_logits(input_ids)
+# prompt = fun.prompt_list(path_prompts_file)[8]
+# list_func, functions_names = fun.functions_list(path_function_file)
+# input_ids =  fun.model.encode(fun.build_prompt(prompt, list_func)).tolist()[0]
+# fun.get_logits(input_ids)
+# function_name_tokens = fun.extract_function_name(functions_names, input_ids)
+# function_name = fun.model.decode(function_name_tokens)
+# fun.extract_function_params(function_name, input_ids)
+
+for prompt in fun.prompt_list(path_prompts_file):
+    list_func, functions_names = fun.functions_list(path_function_file)
+    input_ids = fun.model.encode(fun.build_prompt(prompt, list_func)).tolist()[0]
+    function_name_tokens = fun.extract_function_name(functions_names, input_ids)
+    function_name = fun.model.decode(function_name_tokens)
+    fun.extract_function_params(function_name, input_ids)
+    print("---")
+
+
